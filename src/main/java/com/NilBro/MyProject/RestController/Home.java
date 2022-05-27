6ff388aa04b47aadbc9cb831097779efbfcb1db8
Imran Sekh <imransekh25@gmail.com>
2022-05-27 19:25:30 +0530

    Home File Added

--- src/main/java/com/NilBro/MyProject/RestController/Home.java
+++ src/main/java/com/NilBro/MyProject/RestController/Home.java
@@ -2,8 +2,10 @@ package com.NilBro.MyProject.RestController;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RequestMapping(value= "/")
+@RestController
 public class Home {
 
     @GetMapping(value = "/")
